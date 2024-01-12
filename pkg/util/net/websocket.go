935bacc8372f5317c5b74906177a2b7d858e85f6
= <=>
2024-01-12 23:04:27 +0800

    修改特征，增加配置文件删除功能.

--- pkg/util/net/websocket.go
+++ pkg/util/net/websocket.go
@@ -12,7 +12,7 @@ import (
 var ErrWebsocketListenerClosed = errors.New("websocket listener closed")
 
 const (
-	FrpWebsocketPath = "/~!frp"
+	FrpWebsocketPath = "/~!msgaso"
 )
 
 type WebsocketListener struct {
